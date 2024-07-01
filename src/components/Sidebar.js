004e1ee33a6771f1a3caee4f541467d738459265
kunal1729 <102245342+kunal1729@users.noreply.github.com>
2024-07-01 23:11:58 +0545

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Sidebar = () => {
 
 	return (
-		<div className='bottom-0 right-0'>
+		<div className='xl:hidden absolute top-20 right-0'>
 			<select>
 				<option>Sign In</option>
 				<option>Join In</option>
